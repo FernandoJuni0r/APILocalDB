7fecc1f2f7af5fdaba1690afbde9e777711af019
Pietro Rhyan <pietroqjg@gmail.com>
2022-06-10 23:48:50 -0300

    Mod the hole API

--- server.js
+++ server.js
@@ -1,41 +1,25 @@
-const { send } = require('express/lib/response')
 const http = require('http')
 const port = process.env.PORT || 5000
 const app = require('./app')
 const client = require('./Database')
 const server = http.createServer(app)
 
-
 server.listen(port)
 
 client.connect()
 
 app.get('/login', (req, res) => {
-    const email = req.body.email
-    const pass = req.body.pass
-
-    client.query("Select id from login where email ='" + email + "' and senha='" + pass + "'", (err, result) => {
-        if (!err) {
-            if (result.rows.length != 1) {
-                res.send(false)
-            }
-            else {
-                res.send(true)
-            }
-        }
-        else {
-            console.log(err.message)
-
-        }
+    client.query("Select * from login", (err, result) => {
+      return res.send(result.rows)  
     })
     client.end
 })
 
 app.get('/livros', (req, res)=>{
-
     client.query("select * from catalogo_livros order by id", (err, result)=>{
         if(!err){
             res.send(result.rows)
+            console.log(result.rows)
         }
         else{
             res.send(console.log(err.message))
@@ -45,15 +29,16 @@ app.get('/livros', (req, res)=>{
 })
 
 app.post('/cadastro_livros', (req, res) => {
-
     const titulo = req.body.titulo
     const autor = req.body.autor
     const editora = req.body.editora
     const link = req.body.link
+    const linkImagem = req.body.linkimagem
 
-    client.query("INSERT INTO catalogo_livros (titulo, autor, editora, link) VALUES ( '"+titulo+"' , '"+autor+"' , '"+editora+"', '"+link+"' )", (err, result)=>{
+    client.query("INSERT INTO catalogo_livros (titulo, autor, editora, link, linkimagem) VALUES ( '"+titulo+"' , '"+autor+"' , '"+editora+"', '"+link+"', '"+linkImagem+"' )", (err, result)=>{
         if(!err){
             res.send(true)
+            console.log(titulo)
         }
         else{
             console.log(err.message)
@@ -64,12 +49,12 @@ app.post('/cadastro_livros', (req, res) => {
 })
 
 app.delete('/delete_livro', (req, res)=>{
+    const titulo = req.body.titulo
 
-    const livro = req.body.titulo
-
-    client.query("delete from catalogo_livros where titulo = '"+livro+"'", (err, result)=>{
+    client.query("delete from catalogo_livros where titulo = '"+titulo+"'", (err, result)=>{
         if(!err){
             res.send(true)
+            console.log(titulo)
         }
         else{
             console.log(err.message)
@@ -79,65 +64,20 @@ app.delete('/delete_livro', (req, res)=>{
     client.end
 })
 
-app.put('/update_link', (req, res)=>{
+app.put('/update_livro', (req, res)=>{
 
+    const new_linkimagem = req.body.new_linkimagem
+    const new_titulo = req.body.new_titulo
     const new_link = req.body.new_link
+    const new_autor = req.body.new_autor
+    const new_editora = req.body.new_editora
+
     const cond = req.body.titulo
 
-    client.query("update catalogo_livros set link = '"+new_link+"' where titulo = '"+cond+"'", (err, result)=>{
+    client.query("update catalogo_livros set titulo = '"+new_titulo+"', autor = '"+new_autor+"', editora = '"+new_editora+"', linkimagem = '"+new_linkimagem+"', link = '"+new_link+"' where titulo = '"+cond+"'", (err, result)=>{
         if(!err){
             res.send(true)
-        }
-        else{
-            console.log(err.message)
-            res.send(false)
-        }
-    })
-    client.end
-})
-
-app.put('/update_editora', (req, res)=>{
-
-    const new_editora = req.body.new_editora
-    const cond2 = req.body.titulo
-
-    client.query("update catalogo_livros set editora = '"+new_editora+"' where titulo = '"+cond2+"'", (err, result)=>{
-        if(!err){
-            res.send(true)
-        }
-        else {
-            console.log(err.message)
-            res.send(false)
-        }
-    })
-    client.end
-})
-
-app.put('/update_autor', (req, res)=>{
-
-    const new_autor = req.body.new_autor
-    const cond3 = req.body.titulo
-
-    client.query("update catalogo_livros set autor = '"+new_autor+"' where titulo = '"+cond3+"'", (err, result)=>{
-        if(!err){
-            res.send(true)
-        }
-        else{
-            console.log(err.message)
-            res.send(false)
-        }
-    })
-    client.end
-})
-
-app.put('/update_titulo', (req, res)=>{
-
-    const new_titulo = req.body.new_titulo
-    const cond4 = req.body.link
-
-    client.query("update catalogo_livros set titulo = '"+new_titulo+"' where link = '"+cond4+"'", (err, result)=>{
-        if(!err){
-            res.send(true)
+            console.log(new_titulo)
         }
         else{
             console.log(err.message)
