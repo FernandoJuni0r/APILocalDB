38444f403f05efc7df5d756e7d38d9805fb7675e
Fernando Oliveira Andrade Junior <67484803+FernandoJuni0r@users.noreply.github.com>
2022-06-22 10:43:04 -0300

    Revert "Merge branch 'main' of https://github.com/FernandoJuni0r/APILocalDB"
    This reverts commit 1a2b6b57b855a2f80f9dd8ee5ff2c3fdbe7d95c3, reversing
    changes made to 09101fb88508de4ebe7ac645dfdf6bf5318bc78b.

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
-const cors = require('cors')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+
 
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({ extended: false}))
 app.use(bodyParser.json())
-app.use(cors())
+
+
 
 module.exports = app
--- server.js
+++ server.js
@@ -1,48 +1,61 @@
-const http = require('http')
-const port = process.env.PORT || 5000
-const app = require('./app')
-const client = require('./Database')
-const server = http.createServer(app)
+const { send } = require('express/lib/response')
+const http    = require('http')
+const port    = process.env.PORT || 3000
+const app     = require('./app')
+const client  = require('./Database')
+const server  = http.createServer(app)
+
 
 server.listen(port)
 
 client.connect()
 
-app.get('/login', (req, res) => {
-    const { email } = req.query
-    const { senha } = req.query
-    client.query("Select nome from login where email = '" + email + "' and senha = '" + senha + "'", (err, result) => {
-        if (!err) {
-            return res.send(result.rows)
+app.get('/login', (req, res)=>{
+   const email = req.body.email
+   const pass  = req.body.pass
+
+    client.query("Select id from login where email ='"+email+"' and senha='"+pass+"'", (err, result)=>{
+        if(!err){
+            if (result.rows.length != 1){
+                res.send(false)
+            }
+            else{
+                res.send(true)
+            }
+        }
+        else{
+            console.log(err.message)
+
         }
     })
     client.end
 })
 
-app.get('/livros', (req, res) => {
-    client.query("select * from catalogo_livros order by id", (err, result) => {
-        if (!err) {
+app.get('/livros', (req, res)=>{
+
+    client.query("select * from catalogo_livros order by id", (err, result)=>{
+        if(!err){
             res.send(result.rows)
         }
-        else {
+        else{
             res.send(console.log(err.message))
         }
     })
     client.end
 })
 
-app.post('/cadastro_livros', (req, res) => {
-    const titulo = req.body.titulo
-    const autor = req.body.autor
+app.post('/cadastro_livros', (req, res)=>{
+
+    const titulo  = req.body.titulo
+    const autor   = req.body.autor
     const editora = req.body.editora
-    const link = req.body.link
-    const linkimagem = req.body.linkimagem
+    const link    = req.body.link
 
-    client.query("INSERT INTO catalogo_livros (titulo, autor, editora, link, linkimagem) VALUES ('" + titulo + "' , '" + autor + "', '" + editora + "', '" + link + "', '" + linkimagem + "')", (err, result) => {
-        if (!err) {
+    client.query("INSERT INTO catalogo_livros (titulo, autor, editora, link) VALUES ( '"+titulo+"' , '"+autor+"' , '"+editora+"', '"+link+"' )", (err, result)=>{
+        if(!err){
             res.send(true)
         }
-        else {
+        else{
             console.log(err.message)
             res.send(false)
         }
@@ -50,14 +63,15 @@ app.post('/cadastro_livros', (req, res) => {
     client.end
 })
 
-app.delete('/delete_livro', (req, res) => {
-    const { id } = req.query
+app.delete('/delete_livro', (req, res)=>{
 
-    client.query("delete from catalogo_livros where id = '"+id+"'", (err, result) => {
-        if (!err) {
-            res.send(result.rows)
+    const livro = req.body.titulo
+
+    client.query("delete from catalogo_livros where titulo = '"+livro+"'", (err, result)=>{
+        if(!err){
+            res.send(true)
         }
-        else {
+        else{
             console.log(err.message)
             res.send(false)
         }
@@ -65,22 +79,16 @@ app.delete('/delete_livro', (req, res) => {
     client.end
 })
 
+app.put('/update_link', (req, res)=>{
 
-app.put('/update_livro', (req, res) => {
-
-    const new_linkimagem = req.body.new_linkimagem
-    const new_titulo = req.body.new_titulo
     const new_link = req.body.new_link
-    const new_autor = req.body.new_autor
-    const new_editora = req.body.new_editora
+    const cond = req.body.titulo
 
-    const cond = req.body.id
-
-    client.query("update catalogo_livros set titulo = '" + new_titulo + "', autor = '" + new_autor + "', editora = '" + new_editora + "', linkimagem = '" + new_linkimagem + "', link = '" + new_link + "' where id = '" + cond + "'", (err, result) => {
-        if (!err) {
+    client.query("update catalogo_livros set link = '"+new_link+"' where titulo = '"+cond+"'", (err, result)=>{
+        if(!err){
             res.send(true)
         }
-        else {
+        else{
             console.log(err.message)
             res.send(false)
         }
@@ -88,17 +96,68 @@ app.put('/update_livro', (req, res) => {
     client.end
 })
 
-app.post('/cadastro_usuario', (req, res) => {
+app.put('/update_editora', (req, res)=>{
 
-    const nome = req.body.nome
-    const email = req.body.email
-    const senha = req.body.senha
+    const new_editora = req.body.new_editora
+    const cond2 = req.body.titulo
 
-    client.query("INSERT INTO login (nome, email, senha) VALUES ( '" + nome + "' , '" + email + "' , '" + senha + "' )", (err, result) => {
-        if (!err) {
+    client.query("update catalogo_livros set editora = '"+new_editora+"' where titulo = '"+cond2+"'", (err, result)=>{
+        if(!err){
             res.send(true)
         }
-        else {
+        else{
+            console.log(err.message)
+            res.send(false)
+        }
+    })
+    client.end
+})
+
+app.put('/update_autor', (req, res)=>{
+
+    const new_autor = req.body.new_autor
+    const cond3 = req.body.titulo
+
+    client.query("update catalogo_livros set autor = '"+new_autor+"' where titulo = '"+cond3+"'", (err, result)=>{
+        if(!err){
+            res.send(true)
+        }
+        else{
+            console.log(err.message)
+            res.send(false)
+        }
+    })
+    client.end
+})
+
+app.put('/update_titulo', (req, res)=>{
+
+    const new_titulo = req.body.new_titulo
+    const cond4 = req.body.link
+
+    client.query("update catalogo_livros set titulo = '"+new_titulo+"' where link = '"+cond4+"'", (err, result)=>{
+        if(!err){
+            res.send(true)
+        }
+        else{
+            console.log(err.message)
+            res.send(false)
+        }
+    })
+    client.end
+})
+
+app.post('/cadastro_usuario', (req, res)=>{
+
+    const nome   = req.body.nome
+    const email  = req.body.email
+    const senha  = req.body.senha
+
+    client.query("INSERT INTO login (nome, email, senha) VALUES ( '"+nome+"' , '"+email+"' , '"+senha+"' )", (err, result)=>{
+        if(!err){
+            res.send(true)
+        }
+        else{
             console.log(err.message)
             res.send(false)
         }
